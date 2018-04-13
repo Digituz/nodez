f5ceadabdaff0007f7b7f8c069caacb2838ae32f
Bruno Krebs <bruno.krebs@auth0.com>
2018-04-12 23:44:47 -0300

    startin to create nodez-cli

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 const commandLineArgs = require('command-line-args');
+const https = require('https');
 
 const argsDefinitions = [
   { name: 'command', alias: 'c', type: String, defaultOption: true },
@@ -9,5 +10,19 @@ const argsDefinitions = [
 
 const args = commandLineArgs(argsDefinitions);
 
-console.log(args);
-console.log(`command: ${args.command}; repository: ${args.repository}`);
+if (!args.command || args.command !== 'deploy') {
+  console.error('The only command accepted for now is deploy.');
+  console.error('To use it, run "nodez deploy --repository user-space/repository-name".');
+  console.error('For example: nodez deploy --repository brunokrebs/auth0-web');
+  process.exit(1);
+}
+
+const { command, repository } = args;
+
+const commandDetails = {
+  command,
+  repository,
+}
+
+https.post('http://localhost:3000', commandDetails, console.log)
+  .on("error", console.error);
